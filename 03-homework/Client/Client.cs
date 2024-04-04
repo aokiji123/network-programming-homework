5a87af4dc6537e1c1d4d12e29c0743fe64705ea3
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2024-04-05 00:47:36 +0200

    homework

---
+++ 03-homework/Client/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+
+void StartClient()
+{
+    string serverIP = "127.0.0.1";
+    int port = 12345;
+
+    TcpClient client = new TcpClient(serverIP, port);
+    Console.WriteLine("Connected to the server...");
+
+    NetworkStream stream = client.GetStream();
+    StreamReader reader = new StreamReader(stream);
+    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+    string message;
+    do
+    {
+        Console.Write("You: ");
+        message = Console.ReadLine();
+
+        writer.WriteLine(message);
+
+        string response = reader.ReadLine();
+        Console.WriteLine("Server: " + response);
+
+    } while (message != "Bye");
+
+    stream.Close();
+    client.Close();
+}
+
+StartClient();
---
+++ 03-homework/Server/Server.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+static void StartServer()
+{
+    IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+    int port = 12345;
+
+    TcpListener listener = new TcpListener(ipAddress, port);
+
+    listener.Start();
+    Console.WriteLine("Server is working...");
+
+    TcpClient client = listener.AcceptTcpClient();
+    Console.WriteLine("Client connected");
+
+    NetworkStream stream = client.GetStream();
+    StreamReader reader = new StreamReader(stream);
+    StreamWriter writer = new StreamWriter(stream) 
+    {
+        AutoFlush = true 
+    };
+
+    string message;
+    do
+    {
+        message = reader.ReadLine();
+        Console.WriteLine("Client: " + message);
+
+        string response = GetRandomResponse();
+
+        writer.WriteLine(response);
+        Console.WriteLine("Server: " + response);
+
+    } while (message != "Bye");
+
+    stream.Close();
+    client.Close();
+    listener.Stop();
+}
+
+static string GetRandomResponse()
+{
+    string[] responses = { "Hello!", "I am listening to you", "How can I help you?", "Goodbye!", "What's new?" };
+    Random random = new Random();
+    return responses[random.Next(responses.Length)];
+}
+
+StartServer();
